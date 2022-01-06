7c75891607db06ea6188188a1865f76cb2fbdfd9
Dean-Go-Kr <dean.go.kr@gmail.com>
2022-01-06 23:15:17 +0900

    function parts of go

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+*/
+
+//"naked" return
+func lenAndUpper(name string) (lenght int, uppercase string) {
+	// "defer" runs a code after the function finishes
+	defer fmt.Println("I'm done")
+	lenght = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	// Golang automately set the type
-	name := "deango"
-	name = "dhlee"
-	fmt.Println(name)
+	totalLenght, upperName := lenAndUpper("nico")
+	fmt.Println(totalLenght, upperName)
+
+	repeatMe("nico", "deango", "hello")
 }
